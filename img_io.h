caf66f740c84be6c353f04ebd8c8be921255db84
erictud <erik17242@gmail.com>
2026-02-17 11:16:17 +0200

    Writing image logic + passing img name as cmd parameter

--- img_io.h
+++ img_io.h
@@ -1,11 +1,13 @@
 // returns the matrix of the pixels, along with width and height by using the w and h parameters
-int** read_file(char *f_name, int *w, int *h){
+int** read_img(char *f_name, int *w, int *h){
     char buff[1000];
     int r, g, b, max_col;
+
     // file opening
     FILE *fp = fopen(f_name, "rt");
     if(fp == NULL)
         return NULL;
+
     // reads ppm format and check compatibility
     fgets(buff, 999, fp);
     if(strcmp(buff, "P3\n"))
@@ -16,9 +18,8 @@ int** read_file(char *f_name, int *w, int *h){
     if(num_read != 2)
         return NULL;
 
-    // reads max_col
+    // reads max_color
     fscanf(fp, "%d", &max_col);
-    printf("%d\n", max_col);
     if(max_col > 255)
         return NULL;
 
@@ -33,5 +34,26 @@ int** read_file(char *f_name, int *w, int *h){
             img[i][j*3+2] = b;
         }
     }
+    fclose(fp);
     return img;
+}
+
+// writes the image in a file whose name is passed as a parameter
+int write_img(char *f_name, int **img, int w, int h){
+    // openning file
+    FILE *fp = fopen(f_name, "wt");
+    if(fp == NULL)
+        return 0;
+    
+    // writes img metadata
+    fprintf(fp, "P3\n%d %d\n255\n", w, h);
+    
+    // writes pixels rows
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++)
+            fprintf(fp, "%d %d %d ", img[i][j*3], img[i][j*3+1], img[i][j*3+2]);
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+    return 1;
 }
--- main.c
+++ main.c
@@ -6,11 +6,25 @@
 
 int main(int argc, char **argv){
     int w_img, h_img, **img;
-    // Reading image
-    img = read_file("A-Cat.ppm", &w_img, &h_img);
+
+// Reading image
+    img = read_img(argv[1], &w_img, &h_img);
     if(img == NULL){
         printf("Unexpected error while reading! Check image compatibility and try again!\n");
         return -1;
     }
 
+// Writing image
+    // builds the result image file name (<name>_res.ppm)
+    int l_img_name = strlen(argv[1]);
+    char *res_img_name = (char *)malloc(l_img_name + 5);
+    // copies just the name of the file without the .ppm extension
+    strcpy(res_img_name, strtok(argv[1], ".ppm"));
+    strcat(res_img_name, "_res.ppm");
+    res_img_name[l_img_name + 4] = '\0';
+    int w = write_img(res_img_name, img, w_img, h_img);
+    if(w == 0){
+        printf("Unexpected error while reading! Check image compatibility and try again!\n");
+        return -1;
+    }
 }
